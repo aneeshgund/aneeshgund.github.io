3ea7bac9349ef6a70820a7f08bfd06bf4cc9cc96
aneeshgund <65436866+aneeshgund@users.noreply.github.com>
2020-05-16 10:42:25 +0530

    Update contact_form.js

--- contact_form.js
+++ contact_form.js
@@ -1,24 +1,23 @@
-$(document).ready(function () {
-    $("#submit").click(function () {
-        var rawname = $("#name").val();
-        var rawemail = $("#email").val();
-        var rawmessage = $("#message").val();
-        var rawcontact = $("#contact").val();
+$(document).ready(function() {
+    $("#submit").click(function() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        var contact = $("#contact").val();
         $("#returnmessage").empty(); // To empty previous error/success message.
         // Checking for blank fields.
-        if (rawname == '' || rawemail == '' || rawcontact == '') {
+        if (name == '' || email == '' || contact == '') {
             alert("Please Fill Required Fields");
         } else {
             // Returns successful data submission message when the entered information is stored in database.
-            console.log('Hello world')
             $.post("contact_form.php", {
-                name: rawname,
-                email: rawemail,
-                message: rawmessage,
-                contact: rawcontact
-            }, function (data) {
+                name1: name,
+                email1: email,
+                message1: message,
+                contact1: contact
+            }, function(data) {
                 $("#returnmessage").append(data); // Append returned message to message paragraph.
-                if (data == "Thank you for reaching me, I will contact you soon.") {
+                if (data == "Your Query has been received, We will contact you soon.") {
                     $("#form")[0].reset(); // To reset form fields on success.
                 }
             });
